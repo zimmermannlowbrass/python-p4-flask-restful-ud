f4995b4d9a4a28c1b0ec80a03984406ade39dc8d
Ben Botsford <ben.botsford@flatironschool.com>
2023-03-15 15:02:59 -0500

    fixes #1

--- server/app.py
+++ server/app.py
@@ -80,36 +80,6 @@ class NewsletterByID(Resource):
 
         return response
 
-    def patch(self, id):
-        record = Newsletter.query.filter_by(id=id).first()
-        for attr in request.form:
-            setattr(record, attr, request.form[attr])
-
-        db.session.add(record)
-        db.session.commit()
-
-        response = make_response(
-          record.to_dict(),
-          200  
-        )
-
-        return response
-
-    def delete(self, id):
-        record = Newsletter.query.filter_by(id=id).first()
-
-        db.session.delete(record)
-        db.session.commit()
-
-        response_dict = {"message": "record successfully deleted"}
-
-        response = make_response(
-            response_dict, 
-            200
-        )
-
-        return response
-
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 
